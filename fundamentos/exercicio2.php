de71e8ab6765583270f4473dde0b713b3db24a2a
ana <anahcc@hotmail.com>
2025-08-13 15:28:50 -0300

    Primeiro commit

---
+++ fundamentos/exercicio2.php
@@ -0,0 +1,21 @@
+<?php
+//while   // estrura de repetição
+$x = 0;
+while ($x <= 10) {
+    echo "<br>Numero$x";
+    $x++;
+}
+//for
+for ($x = 0; $x <= 10; $x++) {
+    echo "<br>Numero$x";
+}
+
+//Do while
+$x = 0;
+do{
+    echo "<br>Numero$x";
+    $x++;
+  
+} while ($x <= 10);
+
+?>
