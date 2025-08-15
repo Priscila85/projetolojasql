8e831bda493d15c962e0ed9044f36022cfe87cd6
ANA PRISCILA PAULINO COSTA DE SOUZA <ana.paulino@aluno.senac.rio>
2025-08-15 11:56:24 -0300

    Adiciona pastas fundamentos e v1

--- fundamentos/exercicio3b.php
+++ fundamentos/exercicio3b.php
@@ -1,5 +1,23 @@
 <?php
     $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    $cidade = $_POST["cidade"];
+
+    
 
     echo "Nome digitado: $nome";
+    echo "<br>Idade: $idade";
+    if($cidade == "Cidade"){
+        echo "<br>Cidade não foi informada";
+    }else{
+        echo "<br>Cidade: $cidade";
+    }
+
+     
+
+    if($idade >=18){
+        echo"<br>Vai entrar no Rio Innovation Week";
+    } else{
+        echo "<br> Barrada! Menor de idade!";
+    }
  ?>
