be88f781f25fd0d6901b780b0d920dc4999475a1
ANA PRISCILA PAULINO COSTA DE SOUZA <ana.paulino@aluno.senac.rio>
2025-09-10 12:12:37 -0300

    Adicionei novo commit

--- v1/verificaLogin.php
+++ v1/verificaLogin.php
@@ -8,19 +8,19 @@ $banco = "projetoloja";
 
 $conexao = new mysqli($servidor, $user_banco, $senha_banco, $banco); 
 //capitando dados do formulário de login
-$usuario = $_POST['nome'];
+$usuario = $_POST['nome']; // campo do formulário
 $senha = $_POST['senha'];
 
 //executa a consulta no banco de dados(SQL QUERY)
-$consulta = "SELECT * FROM login WHERE login = '$usuario' AND senha = '$senha'";
+$consulta = "SELECT * FROM login WHERE usuario = '$usuario' AND senha = '$senha'";
 $executa = $conexao->query($consulta);
 
 //verifica se encontrou algum registro
-if ($executa->num_rows) {
-    echo "<br>Usuario logado no sistema";
-    echo "<br>Seja bem vindo, $login";
+if ($executa->num_rows > 0) {
+    echo "Usuário logado no sistema!<br>";
+    echo "Seja bem-vindo, $usuario";
 } else {
-    echo "<br>Usuário ou senha incorretos.";
+    echo "Usuário ou senha incorretos.";
 }
 
 
